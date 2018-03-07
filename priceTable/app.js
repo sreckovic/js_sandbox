43c18a5637d4090190eec2c6c77d0b73da383ed8
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-03-07 13:16:26 +0100

    selected
    selected

--- priceTable/app.js
+++ priceTable/app.js
@@ -1,10 +1,17 @@
 // Add Event Listener
 document.querySelector('.plans').addEventListener('click', function(e) {
-  //console.log(document.querySelectorAll('.plans'));
+  const plans = document.querySelectorAll('.plan');
+  //console.log(document.querySelectorAll('.plan').classList);
+
+  plans.forEach(function(plan) {
+    if (plan.classList.contains('selected')) plan.classList.remove('selected');
+    //console.log(plan.target.classList.contains('selected'));
+  });
+
   const plan = e.target.parentElement;
-  //console.log(plan);
+  console.log(plan);
   const planType = e.target.parentElement.className.replace('plan', '').trim();
-  console.log(planType);
+  //console.log(planType);
 
   if (plan.classList.contains('selected')) {
     console.log('selected already there');
@@ -15,16 +22,23 @@ document.querySelector('.plans').addEventListener('click', function(e) {
 
   switch (planType) {
     case 'rookie':
-      datas('8%', '8%', '8%');
+      // Set users, gb and repos in that order
+      setFeatures('10%', '8%', '8%');
+      break;
     case 'pro':
+      setFeatures('40%', '22%', '12%');
+      break;
     case 'vip':
+      setFeatures('83%', '31%', '22%');
+      break;
     default:
       return;
   }
 });
 
-const datas = function(users, gb, repos) {
+const setFeatures = function(users, gb, repos) {
   document.querySelector('.users .line .fill').style.width = users;
   document.querySelector('.gb .line .fill').style.width = gb;
   document.querySelector('.repos .line .fill').style.width = repos;
+  //console.log('datas() done!');
 };
