a167976df6c403283b2d51ae0b10103425bece20
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-03-07 13:34:56 +0100

    Clean
    Clean

--- priceTable/app.js
+++ priceTable/app.js
@@ -1,21 +1,24 @@
 // Add Event Listener
-document.querySelector('.plans').addEventListener('click', function(e) {
-  const plans = document.querySelectorAll('.plan');
+document.querySelector('.plans').addEventListener('click', function(event) {
+  resetPlans();
+  setPlan(event);
+});
 
-  plans.forEach(function(plan) {
-    if (plan.classList.contains('selected')) plan.classList.remove('selected');
-  });
-
-  const plan = e.target.parentElement;
+const setPlan = function(curr) {
+  // Get selected plan
+  const plan = curr.target.parentElement;
+  // Get plan type
   const planType = plan.className.replace('plan', '').trim();
 
+  // Add selected CSS class
   if (!plan.classList.contains('selected')) {
     plan.classList.add('selected');
   }
 
+  // Set Features according to plan type
   switch (planType) {
     case 'rookie':
-      // Set users, gb and repos in that order
+      // Set users, gb and repos values in that order
       setFeatures('10%', '8%', '8%');
       break;
     case 'pro':
@@ -27,10 +30,21 @@ document.querySelector('.plans').addEventListener('click', function(e) {
     default:
       return;
   }
-});
+};
 
 const setFeatures = function(users, gb, repos) {
   document.querySelector('.users .line .fill').style.width = users;
   document.querySelector('.gb .line .fill').style.width = gb;
   document.querySelector('.repos .line .fill').style.width = repos;
+};
+
+// Reset all plans, we need .plan class attached in HTML
+const resetPlans = function() {
+  const plans = document.querySelectorAll('.plan');
+
+  plans.forEach(function(plan) {
+    if (plan.classList.contains('selected')) {
+      plan.classList.remove('selected');
+    }
+  });
 };
