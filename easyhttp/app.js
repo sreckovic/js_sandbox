86f6039751c37f04f477ea39a9925c97308711e3
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-03-26 17:49:00 +0200

    fetch

--- easyhttp/app.js
+++ easyhttp/app.js
@@ -1,7 +1,61 @@
 const http = new easyHTTP();
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, response) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(
+//   err,
+//   response
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+//   err,
+//   response
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(
+//   err,
+//   response
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(
+  err,
+  response
+) {
   if (err) {
     console.log(err);
   } else {
