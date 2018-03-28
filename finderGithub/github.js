268f56fff6b94823f2134976f6c772c0117d1ce7
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-03-28 16:04:50 +0200

    repos list

--- finderGithub/github.js
+++ finderGithub/github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.clientId = '1217de777fa01fb35cb0';
     this.clientSecret = 'a8f53b47498f2698d83273afc3a160fbff8d7261';
+    this.reposCount = 5;
+    this.reposSort = 'created: asc';
   }
 
   async getUser(user) {
@@ -11,11 +13,21 @@ class Github {
       }&client_secret=${this.clientSecret}`
     );
 
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.reposCount
+      }&sort=${this.reposSort}&client_id=${this.clientId}&client_secret=${
+        this.clientSecret
+      }`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 
     return {
-      //profile: profile
-      profile
+      profile: profile,
+      //profile,
+      repos: repos
     };
   }
 }
--- finderGithub/ui.js
+++ finderGithub/ui.js
@@ -42,8 +42,39 @@ class UI {
     `;
   }
 
+  showRepos(repos) {
+    let output = '';
+    repos.forEach(repo => {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blanl">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+              <span class="badge badge-primary">
+              Stars: ${repo.stargazers_count}</span>
+              <span class="badge badge-primary">
+              Public Gists: ${repo.watchers_count}</span>
+              <span class="badge badge-success">
+              Followers: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    document.getElementById('repos').innerHTML = output;
+  }
+
   showAlert(msg, className) {
     this.clearAlert();
+    // const div = `
+    //   <div class="${className}">${msg}</div>
+    // `;
+
+    // console.log(div);
+
     const div = document.createElement('div');
     div.className = className;
     div.appendChild(document.createTextNode(msg));
