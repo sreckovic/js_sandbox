7ac475a089c2b84621d3b79a02730d84e2ee4aea
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-03-28 13:53:45 +0200

    github finder

--- finderGithub/ui.js
+++ finderGithub/ui.js
@@ -8,13 +8,13 @@ class UI {
     this.profile.innerHTML = `
     <div class="card card-body mb-3">
       <div class="row">
-        <div class="col=md-3">
+        <div class="col-md-3">
           <img class="img-fluid mb-2" src="${user.avatar_url}">
           <a href="${
             user.html_url
-          }" target="_blank" class="btn btn-primary btn-block">View Profile</a>
+          }" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
         </div>
-        <div class="col=md-3">
+        <div class="col-md-9">
           <span class="badge badge-primary">Public Repos: ${
             user.public_repos
           }</span>
@@ -24,7 +24,7 @@ class UI {
           <span class="badge badge-success">Followers: ${user.followers}</span>
           <span class="badge badge-info">Following: ${user.following}</span>
           <br>
-          <ul class="list-group">
+          <ul class="list-group mt-4">
             <li class="list-group-item">Company: ${user.company}</li>
             <li class="list-group-item">Website: ${user.blog}</li>
             <li class="list-group-item">Location: ${user.location}</li>
@@ -33,7 +33,7 @@ class UI {
         </div>
       </div>
     </div>
-    <h3 class="page-heading mb-3">latest Repos</h3>
+    <h3 class="page-heading mb-3">Latest Repos</h3>
     <div id="repos"></div>
     `;
   }
