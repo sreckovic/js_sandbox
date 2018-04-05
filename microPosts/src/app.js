dbfa2acdc1949021c536d9e4ae2bb7d44221aece
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-04-05 14:07:22 +0200

    Posts

---
+++ microPosts/src/app.js
@@ -0,0 +1,54 @@
+import { http } from './http';
+import { ui } from './ui';
+
+// Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
+
+// Listen for submit post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+function getPosts() {
+  http
+    .get('http://localhost:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  };
+
+  // Create Post
+  http
+    .post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
+}
+
+function deletePost(e) {
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+
+    if (confirm('Are you sure?')) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then()
+        .catch();
+    }
+  }
+}
